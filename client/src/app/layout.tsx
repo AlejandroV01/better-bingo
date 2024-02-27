19f1408c35aeb79057598d335bba89dc28e81c94
Alejandro Vera <alexvera0109@gmail.com>
2024-02-27 14:45:50 -0500

    export font bug

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from 'next'
 import { Inter, Poppins } from 'next/font/google'
 import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
-export const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
